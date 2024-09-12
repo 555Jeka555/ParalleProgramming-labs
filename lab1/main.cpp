4f6b3c5d205db6425eee803f769b40ee5bac2646
555Jeka555 <2003antyshev@mail.ru>
2024-09-12 12:16:06 +0300

    Убрал Resume

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -7,11 +7,7 @@ DWORD WINAPI ThreadProc(CONST LPVOID lpParam)
 {
     int threadNumber = *static_cast<int*>(lpParam);
 
-    Sleep(250);
-
     std::cout << "Полезная работа потока " << threadNumber << "\n";
-
-    Sleep(250);
 
     ExitThread(0);
 }
@@ -27,11 +23,6 @@ int _tmain(int argc, _TCHAR* argv[])
     {
         int* threadNum = new int(i + 1);
         handles[i] = CreateThread(NULL, 0, &ThreadProc, threadNum, 0, NULL);
-    }
-
-    for (int i = 0; i < n; i++)
-    {
-        ResumeThread(handles[i]);
     }
 
     WaitForMultipleObjects(n, handles, true, INFINITE);
