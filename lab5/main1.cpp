cb49f4286b43a995a9b2d770bcf528996278b1cb
555Jeka555 <2003antyshev@mail.ru>
2024-11-28 17:45:25 +0300

    Написал 5 лабу

---
+++ lab5/main1.cpp
@@ -0,0 +1,92 @@
+//#include <windows.h>
+//#include <string>
+//#include <iostream>
+//#include "tchar.h"
+//#include <fstream>
+//#include <vector>
+//
+//CRITICAL_SECTION FileLockingCriticalSection;
+//
+//int ReadFromFile() {
+//    EnterCriticalSection(&FileLockingCriticalSection);
+//    std::fstream myfile("balance.txt", std::ios_base::in);
+//    int result;
+//    myfile >> result;
+//    myfile.close();
+//    LeaveCriticalSection(&FileLockingCriticalSection);
+//
+//    return result;
+//}
+//
+//void WriteToFile(int data) {
+//    EnterCriticalSection(&FileLockingCriticalSection);
+//    std::fstream myfile("balance.txt", std::ios_base::out);
+//    myfile << data << std::endl;
+//    myfile.close();
+//    LeaveCriticalSection(&FileLockingCriticalSection);
+//}
+//
+//int GetBalance() {
+//    int balance = ReadFromFile();
+//    return balance;
+//}
+//
+//void Deposit(int money) {
+//    int balance = GetBalance();
+//    balance += money;
+//
+//    WriteToFile(balance);
+//    printf("Balance after deposit: %d\n", balance);
+//}
+//
+//void Withdraw(int money) {
+//    if (GetBalance() < money) {
+//        printf("Cannot withdraw money, balance lower than %d\n", money);
+//        return;
+//    }
+//
+//    Sleep(20);
+//    int balance = GetBalance();
+//    balance -= money;
+//    WriteToFile(balance);
+//    printf("Balance after withdraw: %d\n", balance);
+//}
+//
+//DWORD WINAPI DoDeposit(LPVOID lpParameter) {
+//    Deposit(static_cast<int>(reinterpret_cast<size_t>(lpParameter)));
+//    ExitThread(0);
+//}
+//
+//DWORD WINAPI DoWithdraw(LPVOID lpParameter) {
+//    Withdraw(static_cast<int>(reinterpret_cast<size_t>(lpParameter)));
+//    ExitThread(0);
+//}
+//
+//int main() {
+//    std::vector<HANDLE> handles(50);
+//
+//    InitializeCriticalSection(&FileLockingCriticalSection);
+//
+//    WriteToFile(0);
+//
+//    SetProcessAffinityMask(GetCurrentProcess(), 1);
+//    for (int i = 0; i < 50; i++) {
+//        handles[i] = i % 2 == 0
+//                     ? CreateThread(nullptr, 0, &DoDeposit, reinterpret_cast<LPVOID>(230), CREATE_SUSPENDED,
+//                                    nullptr)
+//                     : CreateThread(nullptr, 0, &DoWithdraw, reinterpret_cast<LPVOID>(1000), CREATE_SUSPENDED,
+//                                    nullptr);
+//        ResumeThread(handles[i]);
+//    }
+//
+//
+//    // ожидание окончания работы двух потоков
+//    WaitForMultipleObjects(50, handles.data(), true, INFINITE);
+//    printf("Final Balance: %d\n", GetBalance());
+//
+//    getchar();
+//
+//    DeleteCriticalSection(&FileLockingCriticalSection);
+//
+//    return 0;
+//}
